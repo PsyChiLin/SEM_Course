c3c52037c870ba45abaf434486641fd0486eaaef
PsyChiLin <psychilinyu@gmail.com>
2018-04-22 20:37:43 +0800

    1.0

--- HW4_Estimation/process.R
+++ HW4_Estimation/process.R
@@ -1,12 +1,15 @@
+# Require the lavaan package
 library(lavaan)
+# Read data into working directory
 dta <- read.csv("HW4_Estimation/data/lower.csv")
+# Data preprocessing
 row.names(dta) <- dta[,1]
 dta <- dta[,-1]
 dta[upper.tri(dta)] <- dta[lower.tri(dta)]
 dta <- as.matrix(dta)
-
+# Define sample size
 N = 403
-
+# Specify model
 model <- '
     ## latent variable definitions
     # endo
@@ -22,22 +25,20 @@ model <- '
     ICO ~ b1 * SEB + b2 * POE
     IEC ~ b3 * ICO
 '
+# Fit the model using ML estimator
 fit <- sem(model, estimator = "ML",
            #likelihood = "wishart",
            sample.nobs = N, sample.cov = dta)
-
+# Show the result
 summary(fit, standardized=TRUE, fit.measures=T)
 capture.output(summary(fit, standardized=TRUE, fit.measures=T), file = "HW4_Estimation/fit.txt")
-
-
+# Produce Figure
 pdf(file = "HW4_Estimation/Fig1_Demo.pdf",width = 7, height = 7)
 semPaths(fit,what = "col",
          nDigits = 3,
          groups = "latents",bg = "white", 
          style = "lisrel",layout = "circle")
 dev.off()
-
-
 pdf(file = "HW4_Estimation/Fig2_Rst.pdf",width = 7, height = 7)
 semPaths(fit,what = "col",whatLabels = "std",
          nDigits = 3,
