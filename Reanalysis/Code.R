cd1896b3f19bdd50eb9c23a345a3a923af0d1484
PsyChiLin <psychilinyu@gmail.com>
2018-04-24 21:33:47 +0800

    Re test

--- Reanalysis/Code.R
+++ Reanalysis/Code.R
@@ -19,7 +19,8 @@ dtacor <- getCov(lower, names=c("FM_TL","BM_TL", "FM_TI", "BM_TI","PSCL","WF","I
 dtacov <- cor2cov(dtacor,c(17.3,17.1,8.4,7.5,5.7,1.7,2.1,2.0))
 # Define sample size
 N = 75
-# Specify model
+
+# Specify model: Basic Model
 BM1 <- '
     # latent variable definitions
     EVP =~ 1 * FM_TL + BM_TL + FM_TI + BM_TI
@@ -35,12 +36,12 @@ fit_BM <- sem(BM1, estimator = "ML",
 summary(fit_BM, standardized=TRUE, fit.measures=T)
 capture.output(summary(fit_BM, standardized=TRUE, fit.measures=T), file = "fit_BM.txt")
 # Produce Figure
-pdf(file = "Fig1_BM_Demo.pdf",width = 7, height = 7)
-semPaths(fit_BM,what = "col",
-         nDigits = 3,
-         groups = "latents",bg = "white", 
-         style = "lisrel",layout = "circle")
-dev.off()
+# pdf(file = "Fig1_BM_Demo.pdf",width = 7, height = 7)
+# semPaths(fit_BM,what = "col",
+#          nDigits = 3,
+#          groups = "latents",bg = "white", 
+#          style = "lisrel",layout = "circle")
+# dev.off()
 pdf(file = "Fig2_BM_Rst.pdf",width = 7, height = 7)
 semPaths(fit_BM,what = "col",whatLabels = "std",
          nDigits = 3,
@@ -65,8 +66,69 @@ fit_MM1 <- sem(MM1, estimator = "ML",
 # Show the result
 summary(fit_MM1, standardized=TRUE, fit.measures=T)
 capture.output(summary(fit_MM1, standardized=TRUE, fit.measures=T), file = "fit_MM1.txt")
-
+# Figure
+# pdf(file = "Fig3_MM1_Demo.pdf",width = 7, height = 7)
+# semPaths(fit_MM1,what = "col",
+#          nDigits = 3,
+#          groups = "latents",bg = "white", 
+#          style = "lisrel",layout = "circle")
+# dev.off()
+pdf(file = "Fig4_MM1_Rst.pdf",width = 7, height = 7)
 semPaths(fit_MM1,what = "col",whatLabels = "std",
          nDigits = 3,
          groups = "latents",bg = "white", 
          style = "lisrel")
+dev.off()
+
+## MM2 model : use PATH rather than LV
+MM2 <- '
+    # latent variable definitions
+    EVP =~ 1 * FM_TL + BM_TL + FM_TI + BM_TI
+    FS  =~ 1 * WF + IL + SF
+    # regressions
+    FS ~ EVP + PSCL
+    PSCL ~ EVP
+'
+# Fit the model using ML estimator
+fit_MM2 <- sem(MM2, estimator = "ML",
+               #likelihood = "wishart",
+               sample.nobs = N, sample.cov = dtacov)
+# Show the result
+summary(fit_MM2, standardized=TRUE, fit.measures=T)
+capture.output(summary(fit_MM2, standardized=TRUE, fit.measures=T), file = "fit_MM2.txt")
+# Figure
+# pdf(file = "Fig5_MM2_Demo.pdf",width = 7, height = 7)
+# semPaths(fit_MM2,what = "col",
+#          nDigits = 3,
+#          groups = "latents",bg = "white", 
+#          style = "lisrel",layout = "circle")
+# dev.off()
+pdf(file = "Fig6_MM2_Rst.pdf",width = 7, height = 7)
+semPaths(fit_MM2,what = "col",whatLabels = "std",
+         nDigits = 3,
+         groups = "latents",bg = "white", 
+         style = "lisrel")
+dev.off()
+
+## MM2 & MM1 are two equ models
+
+# Two Step 
+CFApart <- '
+    EVP =~ 1 * FM_TL + BM_TL + FM_TI + BM_TI
+    FS  =~ 1 * WF + IL + SF
+    SP  =~ 1 * PSCL
+'
+# Fit the model using ML estimator
+fit_CFApart <- sem(CFApart, estimator = "ML",
+               #likelihood = "wishart",
+               sample.nobs = N, sample.cov = dtacov)
+# Show the result
+summary(fit_CFApart, standardized=TRUE, fit.measures=T)
+capture.output(summary(fit_CFApart, standardized=TRUE, fit.measures=T), file = "fit_CFApart.txt")
+
+pdf(file = "Fig7_CFApart_Rst.pdf",width = 7, height = 7)
+semPaths(fit_CFApart,what = "col",whatLabels = "std",
+         nDigits = 3,
+         groups = "latents",bg = "white", 
+         style = "lisrel")
+dev.off()
